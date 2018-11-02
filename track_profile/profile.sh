3299bd4cf683491487a6bcdc8b024096359033b8
Pengzhong <zhongpenggeo@gmail.com>
2018-11-02 21:44:17 +0800

    update scale

--- track_profile/profile.sh
+++ track_profile/profile.sh
@@ -21,12 +21,13 @@ gmt grdgradient $GRD -A45 -Nt -G$ILLU
 gmt psxy -$JJ -$RR -T -K >$PS
 gmt grdimage $GRD -$JJ -$RR -I$ILLU -C$CPT -K -O -P >>$PS 
 gawk '{print $1,$2}' $DAT|gmt psxy -$JJ -$RR -B5 -BWnSe -W2p,red -K -O >>$PS
+gmt psscale -Dx8.5c/0.2c+w10c/0.3c -C$CPT -I  -G-4000/1000 -Bx1000 -By+l"m" -K -O >>$PS
 gmt grdtrack $DAT -G$GRD|gawk '{print $3,$4}' >$PRO_DAT
 #求最大小值
 X_MAX=`gawk 'END{print $1}' $PRO_DAT`
 Y_MIN=`cat $PRO_DAT|gawk 'NR==1{min=$2;next}{min=min<$2?min:$2}END{print min}'`
 Y_MAX=`cat $PRO_DAT|gawk 'NR==1{max=$2;next}{max=max>$2?max:$2}END{print max}'`
 #Y_RANGE=`gmt grdinfo -T1 $GRD|gawk '{print substr($0,3)}'`
-gmt psxy -Y12c $PRO_DAT -$JJP  -R$X_MIN/$X_MAX/$Y_MIN/$Y_MAX -Ba1000 -BSW  --MAP_FRAME_TYPE=graph -K -O >>$PS
+gmt psxy -Y12c $PRO_DAT -$JJP  -R$X_MIN/$X_MAX/$Y_MIN/$Y_MAX -Ba1000 -BSW -W2p --MAP_FRAME_TYPE=graph -K -O >>$PS
 gmt psxy -$JJ -$RR -T -O >>$PS
 gmt psconvert $PS -Tj -A
